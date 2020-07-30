de7edc4d2a8bb9ccbe0d83e9674e2bfbdf113f12
Ryan Lee <gxlhybh@gmail.com>
2020-07-31 03:55:58 +0800

    [Fix] Release 2.1.1 Fix registry.json error

--- src/env/build-env.ts
+++ src/env/build-env.ts
@@ -1,3 +1,27 @@
 import fs from "fs";
 import path from "path";
-global.config = JSON.parse(fs.readFileSync(path.resolve(process.cwd(), "config", "registry.json"), "utf-8"));
+
+const baseRegistry = {
+  registry: []
+}
+
+function buildRegistryPath () {
+  return path.resolve(process.cwd(), "config", "registry.json");
+}
+
+function attachRegistry () {
+  if (fs.existsSync(buildRegistryPath())) {
+    // do nothing
+  }
+  else {
+    fs.mkdirSync(path.dirname(buildRegistryPath()));
+    fs.writeFileSync(buildRegistryPath(), JSON.stringify(baseRegistry));
+  }
+}
+
+function buildEnv () {
+  attachRegistry();
+  global.config = JSON.parse(fs.readFileSync(buildRegistryPath(), "utf-8"));
+}
+
+buildEnv();
